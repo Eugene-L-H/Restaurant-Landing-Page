0faf036d2dc74e35304a194d7071da12cff09d72
Eugene Helland <eugenehelland@mail.com>
2022-01-15 12:52:27 -0700

    Fixed typo in webpack.config.js, and switched style link to script link in index.html. JS is working now.

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,9 @@
-const path = require('path');const { mainModule } = require('process');
+const path = require('path');
 
 module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'main.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
   },
 };
