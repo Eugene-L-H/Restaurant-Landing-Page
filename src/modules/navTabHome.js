adfa06633a2fd234117ff0e0f99464457b3907a9
Eugene Helland <eugenehelland@mail.com>
2022-01-19 22:00:43 -0700

    Some minor styling improvements; the menu now takes up the full screen and the nav buttons are larger on smaller devices. An attempt at fixing an issue with the nav-menu spilling out of the header as was noticed during testing on an iphoneX.

--- src/modules/navTabHome.js
+++ src/modules/navTabHome.js
@@ -3,7 +3,7 @@ export const navTabHome = () => {
   description.classList.add('description');
 
   let para = document.createElement('p');
-  para.textContent = 'Welcome to Cafe Bistro! Join us and our friendly staff for some of our delicious locally procured cuisine paired with one of our in-house craft beers. Enjoy a cup of ethically sourced organic coffee and embrace our relaxed atmosphere. See you soon!'
+  para.textContent = 'Welcome to Cafe Bistro! Join us for some of our delicious locally procured cuisine paired with one of our in-house craft beers. Enjoy a cup of ethically sourced organic coffee and embrace our relaxed atmosphere. See you soon!'
 
   description.append(para);
   return description;
